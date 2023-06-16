853c2a7e46cbdf0b46c7b4a1a03192f55bcbfb5f
oestrangeiro <mateus12092004@gmail.com>
2023-06-16 20:33:33 -0300

    implementacao total da logica e parcial da UI ao que tange o trabalho do joao vitor

---
+++ src/main/java/com/mycompany/equacao2/EQUACAO2.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.equacao2;
+
+/**
+ *
+ * @author Alunos
+ */
+public class EQUACAO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
